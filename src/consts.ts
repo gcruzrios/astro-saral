e689b7ab0f5c043b48f71f7812b45ac3a82eb555
gcruzrios <gcruzr@gmail.com>
2026-01-26 12:46:56 -0600

    first commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,31 +1,26 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Saral Theme'
-export const SITE_DESCRIPTION =
-	'A simple theme for personal blog sites, created for Astro framework'
+export const SITE_TITLE = 'Greivin Cruz Ríos'
+export const SITE_DESCRIPTION = 'JavaScript FullStack Developer & AI Enthusiast'
 
 // If you deploy to a subdirectory, set the `WEBSITE_BASE` here.
 // e.g. '/blog' if you are deploying to example.com/blog
 // Leave it as an empty string if you are deploying to a root domain (e.g. example.com)
-export const SITE_BASE = '/saral-theme-astro'
+export const SITE_BASE = '/'
 
 export const SocialLinks = [
 	{
 		name: 'GitHub',
-		url: 'https://github.com/yashjawale',
-	},
-	{
-		name: 'LinkedIn',
-		url: 'https://www.linkedin.com/in/yashjawale/',
+		url: 'https://github.com/gcruzrios',
 	},
 	{
 		name: 'X / Twitter',
-		url: 'https://twitter.com/imyashjawale',
+		url: 'https://x.com/gcruzrios',
 	},
 	{
-		name: 'Instagram',
-		url: 'https://instagram.com/imyashjawale',
+		name: 'Website',
+		url: 'http://www.greiv.in',
 	},
 ]
 
